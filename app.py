568fd156e6bca9ef725d996e0fb684a4416a52f0
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-06-08 22:41:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,12 +173,6 @@ if __name__=='__main__':
 
 	updater = Updater(TOKEN)
 
-
-
-	#logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
-
-	#logger = logging.getLogger(__name__)
-
 	updater = Updater(token=TOKEN)
 	
 	dispatcher = updater.dispatcher
